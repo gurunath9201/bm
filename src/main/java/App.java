13b1a86381b244c59a2ffc33b1b4657cc2c89859
gurunath9201 <gurunathmule825@gmail.com>
2025-11-24 23:21:59 +0530

    Simple Blog

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Blog Website Build Successful!");
+        System.out.println("Open index.html to view the site.");
+    }
+}
